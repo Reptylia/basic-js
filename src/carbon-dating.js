92e0247c902c8649440455c4b0e40edfe6a554a4
Reptylia <repty.alliance@gmail.com>
2021-09-15 19:19:15 +0300

    feat: upd

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,7 +18,7 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 export default function dateSample(sampleActivity) {
-  throw new NotImplementedError('Not implemented');
+  
   // remove line with error and write your code here
   if(typeof(sampleActivity) != "number") {
     return false;
